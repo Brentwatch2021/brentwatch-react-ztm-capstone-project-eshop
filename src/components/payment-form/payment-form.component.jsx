b358a76cd21a1a085bdc494f8b5b3200c279c550
Brentwatch2021 <brentwatch2021@gmail.com>
2023-07-09 13:43:27 +0200

    Updated the payment pk to test.

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,6 +1,6 @@
 import { CardElement,useStripe,useElements } from "@stripe/react-stripe-js";
 import { FormContainer, PaymentButton, PaymentFormContainer } from "./payment-form.styles";
-import axios from 'axios'
+
 
 const PaymentForm = () => 
 {
@@ -14,14 +14,14 @@ const PaymentForm = () =>
             return;
         }
 
-        const response = await fetch('/.netlify/functions/create-payment-intent',{
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ amount: 10000 })
-        }).then(res => res.json())
-        .catch(error => console.log(error));
+        // const response = await fetch('/.netlify/functions/create-payment-intent',{
+        //     method: 'post',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({ amount: 10000 })
+        // }).then(res => res.json())
+        // .catch(error => console.log(error));
         
         // client secret links is used for the payment to link the intent to the 
         // payment
