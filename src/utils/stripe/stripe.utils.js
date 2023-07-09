b039184992277c67b27186a9d3bbe21a6d028d37
Brentwatch2021 <brentwatch2021@gmail.com>
2023-07-09 13:57:32 +0200

    Updated the PK Key.

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -4,8 +4,8 @@ import { loadStripe } from "@stripe/stripe-js"
 // For Test purposes we will hard code the publish key to
 // see if reason for the card component not loading 
 // is due to the fact that the pubisheble key cant be found in the .env file
-// export const stripePromise = loadStripe(
-//     process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
-// );
+export const stripePromise = loadStripe(
+    process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+);
 
- export const stripePromise = loadStripe("pk_test_51NRBAJFvASA8S8ybt3tvr7fi5zYhYGp8n83L9ighMTIyOGFRhchQmVu1owbdKTv4EphDEYdOQS1JVOJh3xRvmO7J000Nu254z8");
+ //export const stripePromise = loadStripe("pk_test_51NRBAJFvASA8S8ybt3tvr7fi5zYhYGp8n83L9ighMTIyOGFRhchQmVu1owbdKTv4EphDEYdOQS1JVOJh3xRvmO7J000Nu254z8");
